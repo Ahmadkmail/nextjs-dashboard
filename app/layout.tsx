1dcd98c1e859947cd6c1848e2317dc12e663990b
ahmad kmail <ahmad.kmail19996@gmail.com>
2024-07-28 15:30:24 +0300

    init the project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,22 @@
+
+import '@/app/ui/global.css'
+/**
+ * RootLayout is the root layout component for the entire application.
+ * It wraps the children with the HTML and BODY tags.
+ *
+ * @param {Object} props - The props object.
+ * @param {React.ReactNode} props.children - The children to be rendered.
+ * @return {JSX.Element} The root layout component.
+ */
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
-}) {
+}): JSX.Element {
+  // Render the children wrapped in the HTML and BODY tags.
   return (
     <html lang="en">
+      {/* The BODY tag contains the children to be rendered. */}
       <body>{children}</body>
     </html>
   );
